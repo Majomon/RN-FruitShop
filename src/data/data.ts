ac80188b1a31d83693eaf10e64bb7b13bbef5d36
Mauri <mauri.monzon91@gmail.com>
2024-08-31 20:20:15 -0300

    Renderizado y estilizado las categorias

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,9 +1,16 @@
-export const categories: string[] = [
-  'Orange',
+export const categoriesFruits: string[] = [
+  'Naranjas',
   'Bananas',
-  'Pineapple',
-  'Apple',
-  'Cherry',
+  'Piñas',
+  'Manzanas',
+  'Cerezas',
+];
+
+export const categoriesVegetables: string[] = [
+  'Zanahoria',
+  'Brócoli',
+  'Espinaca',
+  'Tomate',
 ];
 
 interface FeaturedItem {
@@ -12,18 +19,18 @@ interface FeaturedItem {
   star: number;
   desc: string;
   shadow: string;
-  image: any;
+  // image: any;
   color: (opacity: number) => string;
 }
 
-const featuredFruits: FeaturedItem[] = [
+export const featuredFruits: FeaturedItem[] = [
   {
     name: 'Manzana',
     price: '1.50',
     star: 4.5,
     desc: 'Una fruta dulce y crujiente.',
     shadow: 'rgba(255, 0, 0, 0.5)',
-    image: require('./assets/images/apple.png'),
+    // image: require('./assets/images/apple.png'),
     color: opacity => `rgba(255, 0, 0, ${opacity})`,
   },
   {
@@ -32,8 +39,73 @@ const featuredFruits: FeaturedItem[] = [
     star: 4.7,
     desc: 'Una fruta suave y dulce.',
     shadow: 'rgba(255, 255, 0, 0.5)',
-    image: require('./assets/images/banana.png'),
+    // image: require('./assets/images/banana.png'),
     color: opacity => `rgba(255, 255, 0, ${opacity})`,
   },
-  // Agrega más frutas según sea necesario
+  {
+    name: 'Piña',
+    price: '2.00',
+    star: 4.8,
+    desc: 'Una fruta tropical y jugosa.',
+    shadow: 'rgba(255, 223, 0, 0.5)',
+    // image: require('./assets/images/pineapple.png'),
+    color: opacity => `rgba(255, 223, 0, ${opacity})`,
+  },
+  {
+    name: 'Cereza',
+    price: '3.00',
+    star: 4.9,
+    desc: 'Una fruta pequeña y dulce.',
+    shadow: 'rgba(220, 20, 60, 0.5)',
+    // image: require('./assets/images/cherry.png'),
+    color: opacity => `rgba(220, 20, 60, ${opacity})`,
+  },
+  {
+    name: 'Naranja',
+    price: '1.20',
+    star: 4.6,
+    desc: 'Una fruta cítrica y refrescante.',
+    shadow: 'rgba(255, 165, 0, 0.5)',
+    // image: require('./assets/images/orange.png'),
+    color: opacity => `rgba(255, 165, 0, ${opacity})`,
+  },
+];
+
+export const featuredVegetables: FeaturedItem[] = [
+  {
+    name: 'Zanahoria',
+    price: '0.80',
+    star: 4.3,
+    desc: 'Una verdura crujiente y dulce.',
+    shadow: 'rgba(255, 165, 0, 0.5)',
+    // image: require('./assets/images/carrot.png'),
+    color: opacity => `rgba(255, 165, 0, ${opacity})`,
+  },
+  {
+    name: 'Brócoli',
+    price: '1.20',
+    star: 4.6,
+    desc: 'Una verdura verde y saludable.',
+    shadow: 'rgba(0, 128, 0, 0.5)',
+    // image: require('./assets/images/broccoli.png'),
+    color: opacity => `rgba(0, 128, 0, ${opacity})`,
+  },
+  {
+    name: 'Espinaca',
+    price: '1.00',
+    star: 4.4,
+    desc: 'Una verdura verde y nutritiva.',
+    shadow: 'rgba(34, 139, 34, 0.5)',
+    // image: require('./assets/images/spinach.png'),
+    color: opacity => `rgba(34, 139, 34, ${opacity})`,
+  },
+  {
+    name: 'Tomate',
+    price: '0.90',
+    star: 4.5,
+    desc: 'Una verdura jugosa y versátil.',
+    shadow: 'rgba(255, 99, 71, 0.5)',
+    // image: require('./assets/images/tomato.png'),
+    color: opacity => `rgba(255, 99, 71, ${opacity})`,
+  },
 ];
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Pressable, ScrollView, Text, View} from 'react-native';
 import {MenuTop} from '../../components/home';
 import {colors} from '../../../config/theme/theme';
+import {categoriesFruits} from '../../../data/data';
 
 export const HomeScreen = () => {
+  const [activeCategory, setActiveCategory] = useState('Naranjas');
   return (
     <View className="flex-1 bg-orange-50 my-1">
       <MenuTop />
@@ -21,7 +23,28 @@ export const HomeScreen = () => {
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
-          className="mt-8 px-5"></ScrollView>
+          className="mt-8 px-5">
+          {categoriesFruits.map((category, index) => {
+            let isActive = category == activeCategory;
+            let textClass = `text-xl ${isActive ? 'font-bold' : ''}`;
+
+            return (
+              <Pressable
+                key={index}
+                className="mr-8 relative"
+                onPress={() => setActiveCategory(category)}>
+                <Text className={textClass} style={{color: colors.text}}>
+                  {category}
+                </Text>
+                {isActive ? (
+                  <Text className="font-extrabold text-orange-400 -mt-3 ml-2">
+                    __ _
+                  </Text>
+                ) : null}
+              </Pressable>
+            );
+          })}
+        </ScrollView>
       </View>
     </View>
   );
