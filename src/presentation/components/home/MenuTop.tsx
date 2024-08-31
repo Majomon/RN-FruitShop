9e9882389ef663cec19e9ea0daa02c50030e0266
Mauri <mauri.monzon91@gmail.com>
2024-08-30 22:39:21 -0300

    MenuTop

---
+++ src/presentation/components/home/MenuTop.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const MenuTop = () => {
+  return (
+    <View>
+      <Text>MenuTop</Text>
+    </View>
+  );
+};
--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -10,7 +10,7 @@ const Stack = createStackNavigator<RootStackParams>();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
     </Stack.Navigator>
   );
