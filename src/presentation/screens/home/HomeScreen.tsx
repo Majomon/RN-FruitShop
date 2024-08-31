52f29287c7c345e83787dccd4d49e90f68f13ce5
Mauri <mauri.monzon91@gmail.com>
2024-08-30 22:48:34 -0300

    Tailwind

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {MyIcon} from '../../components/ui/MyIcon';
+import { MyIcon } from '../../components/ui/MyIcon';
+
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
-      <MyIcon name="accessibility-outline" />
+      <MyIcon name='accessibility-outline'/>
     </View>
   );
 };
